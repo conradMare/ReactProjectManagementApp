8ccca618aee50b8b651f49ab1f9673cd131325db
conradMare <158014474+conradMare@users.noreply.github.com>
2024-11-11 09:23:50 +0200

    2024/11/11(am)
    React Project Management Application Deployed via Netlify

---
+++ project-management-app/src/components/NewTask.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+export default function NewTask({ onAdd }) {
+
+    const [enteredTask, setEnteredTask] = useState('');
+
+    function handleChange(e) {
+        setEnteredTask(e.target.value);
+    }
+
+    function handleClick() {
+        if (enteredTask.trim() === '') {
+            return;
+        }
+        onAdd(enteredTask);
+        setEnteredTask('');
+    }
+
+    return (
+        <div className="flex items-center gap-4">
+            <input
+                type="text"
+                className="w-64 px-2 py-1 rounded-sm bg-stone-300"
+                onChange={handleChange}
+                value={enteredTask}
+            />
+            <button
+                className="p-2 rounded-md bg-stone-500 text-stone-100 hover:text-stone-950"
+                onClick={handleClick}
+            >
+                Add Task
+            </button>
+        </div>
+    );
+}
